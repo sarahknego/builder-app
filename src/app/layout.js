0634927e414cec820b0ee3b37e870f432579d860
sarahknego <sarah.knego@yahoo.com>
2024-09-19 15:00:47 -0500

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,6 +20,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <link rel="icon" type="image/x-icon" href="/favicon.ico"></link>
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
